2c07401a1330fc2d6297b8c0760706017a673241
Christopher Hicks <chicks.net@gmail.com>
2016-02-15 18:32:20 -0800

    first pass at starting Garry's Mod automatically :bowling:

---
+++ StartGmod.sikuli/StartGmod.py
@@ -0,0 +1,25 @@
+# start Garry's Mod
+
+import time
+
+# switch to Steam
+switchApp("Steam")
+click("1455586267055.png")
+# click("recent.png")
+click("1455586675687.png")
+click("1455586694710.png")
+
+# wait
+time.sleep(10)
+
+switchApp("Garry's Mod")
+wait("1455586899748.png",20)
+click("1455586899748.png")
+click("1455586932620.png")
+click("1455586944860.png")
+
+
+
+
+
+
