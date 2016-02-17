2480aa9cf766d9361c2a3335714d024f4bd5b344
Christopher Hicks <chicks.net@gmail.com>
2016-02-17 15:32:12 -0800

    improve reliability :baseball:

--- StartGmod.sikuli/StartGmod.py
+++ StartGmod.sikuli/StartGmod.py
@@ -10,10 +10,11 @@ click("garrys_mod.png")
 click("steam_play.png")
 
 # wait
-time.sleep(10)
+time.sleep(15)
 
-switchApp("Garry's Mod")
+#switchApp("Garry's Mod")
 wait("start_new_game.png",20)
+time.sleep(2)
 click("start_new_game.png")
 click("gm_construct.png")
 click("start_game.png")
