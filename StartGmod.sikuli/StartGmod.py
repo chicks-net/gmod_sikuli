6d9478651bb3afb845eb4f1312007003d6497e52
Christopher Hicks <chicks.net@gmail.com>
2016-02-17 15:55:53 -0800

    start Gmod more reliably still :8ball:

--- StartGmod.sikuli/StartGmod.py
+++ StartGmod.sikuli/StartGmod.py
@@ -16,8 +16,21 @@ time.sleep(15)
 wait("start_new_game.png",20)
 time.sleep(2)
 click("start_new_game.png")
-click("gm_construct.png")
+
+try:
+    click("gm_construct.png")
+except:
+    time.sleep(2)
+    click("start_new_game.png")
+
 click("start_game.png")
+
+# wait for level to load
+wait(Pattern("construct_tower.png").similar(0.61),15)
+
+for i in range(20):
+    type("q")
+    time.sleep(0.2)
 
 
 
