2368499129e3a548317694f0ef00295661420d3b
Christopher Hicks <chicks.net@gmail.com>
2016-02-17 18:22:47 -0800

    let Start focus on starting :gun:

--- StartGmod.sikuli/StartGmod.py
+++ StartGmod.sikuli/StartGmod.py
@@ -12,7 +12,6 @@ click("steam_play.png")
 # wait
 time.sleep(15)
 
-#switchApp("Garry's Mod")
 wait("start_new_game.png",20)
 time.sleep(2)
 click("start_new_game.png")
@@ -27,10 +26,6 @@ click("start_game.png")
 
 # wait for level to load
 wait(Pattern("construct_tower.png").similar(0.61),15)
-
-for i in range(20):
-    type("q")
-    time.sleep(0.2)
 
 
 
